a8c0d1d4f57bde9e45a6e5edc73e8fcdac008dd4
kanami_suzuki <58844108+kanami-suzuki@users.noreply.github.com>
2024-12-09 22:04:19 +0900

    コンポーネントの見た目作成

--- react-tutorial2/todo-sample-empty-main/src/pages/practice/01.tsx
+++ react-tutorial2/todo-sample-empty-main/src/pages/practice/01.tsx
@@ -1,7 +1,29 @@
 import { NextPage } from 'next';
 
 const Page: NextPage = () => {
-  return <div>こんにちは</div>;
+  return( <div className='mx-auto max-w-4xl mt-10'>
+    <div className='grid grid-cols-3 gap-x-4'>
+      <div className='shadow-xl p-6'>
+        <h3 className='text-xl font-bold'>タイトル</h3>
+        <p className='mt-4 text-base'>
+          コンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツ
+        </p>
+      </div>
+      <div className='shadow-xl p-6'>
+        <h3 className='text-xl font-bold'>タイトル</h3>
+        <p className='mt-4 text-base'>
+          コンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツ
+        </p>
+      </div>
+      <div className='shadow-xl p-6'>
+        <h3 className='text-xl font-bold'>タイトル</h3>
+        <p className='mt-4 text-base'>
+          コンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツ
+        </p>
+      </div>
+    </div>
+  </div>
+  );
 };
 
 export default Page;
