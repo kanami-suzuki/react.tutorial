af2b2c5f953e0b581ceb1b7aacc607cd2fc0d762
kanami_suzuki <58844108+kanami-suzuki@users.noreply.github.com>
2024-12-11 21:12:56 +0900

    コンポーネントを分ける

---
+++ react-tutorial2/todo-sample-empty-main/src/components/ThreeBoxContent.tsx
@@ -0,0 +1,24 @@
+const ThreeBoxContent = (): JSX.Element => {
+  return (<div className='grid grid-cols-3 gap-x-4'>
+    <div className='shadow-xl p-6'>
+      <h3 className='text-xl font-bold'>タイトル</h3>
+      <p className='mt-4 text-base'>
+        コンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツ
+      </p>
+    </div>
+    <div className='shadow-xl p-6'>
+      <h3 className='text-xl font-bold'>タイトル</h3>
+      <p className='mt-4 text-base'>
+        コンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツ
+      </p>
+    </div>
+    <div className='shadow-xl p-6'>
+      <h3 className='text-xl font-bold'>タイトル</h3>
+      <p className='mt-4 text-base'>
+        コンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツ
+      </p>
+    </div>
+  </div>);
+}
+
+export default ThreeBoxContent;
--- react-tutorial2/todo-sample-empty-main/src/pages/practice/01.tsx
+++ react-tutorial2/todo-sample-empty-main/src/pages/practice/01.tsx
@@ -1,27 +1,10 @@
+import ThreeBoxContent from '@/components/ThreeBoxContent';
 import { NextPage } from 'next';
 
 const Page: NextPage = () => {
-  return( <div className='mx-auto max-w-4xl mt-10'>
-    <div className='grid grid-cols-3 gap-x-4'>
-      <div className='shadow-xl p-6'>
-        <h3 className='text-xl font-bold'>タイトル</h3>
-        <p className='mt-4 text-base'>
-          コンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツ
-        </p>
-      </div>
-      <div className='shadow-xl p-6'>
-        <h3 className='text-xl font-bold'>タイトル</h3>
-        <p className='mt-4 text-base'>
-          コンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツ
-        </p>
-      </div>
-      <div className='shadow-xl p-6'>
-        <h3 className='text-xl font-bold'>タイトル</h3>
-        <p className='mt-4 text-base'>
-          コンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツコンテンツ
-        </p>
-      </div>
-    </div>
+  return( 
+  <div className='mx-auto max-w-4xl mt-10'>
+    <ThreeBoxContent />
   </div>
   );
 };
