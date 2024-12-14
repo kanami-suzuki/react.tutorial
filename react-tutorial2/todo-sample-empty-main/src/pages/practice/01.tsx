ab9e1e10883e61f952fbbee3a2a637d3caa1cdb5
kanami_suzuki <58844108+kanami-suzuki@users.noreply.github.com>
2024-12-14 23:58:45 +0900

    state

--- react-tutorial2/todo-sample-empty-main/src/pages/practice/01.tsx
+++ react-tutorial2/todo-sample-empty-main/src/pages/practice/01.tsx
@@ -1,18 +1,29 @@
-import ThreeBoxContent from '@/components/ThreeBoxContent';
 import { NextPage } from 'next';
+import { useState } from 'react';
+
+import Button from '@/components/common/parts/Button';
 
 const Page: NextPage = () => {
+  const [count, setCount] = useState(0);
+  const onClickCountUp = () => {
+    setCount((prevState) => prevState + 1);
+    setCount((prevState) => prevState + 1);
+    setCount((prevState) => prevState + 1);
+  }
+
   return( 
-  <div className='mx-auto max-w-4xl mt-10'>
-    <ThreeBoxContent 
-    title1='これはタイトルです。'
-    content1='これはコンテンツです。これはコンテンツです。これはコンテンツです。これはコンテンツです。これはコンテンツです。これはコンテンツです。これはコンテンツです。'
-    title2='これはタイトルです!'
-    content2='これはコンテンツです。これはコンテンツです。これはコンテンツです。これはコンテンツです。これはコンテンツです。これはコンテンツです。これはコンテンツです。'
-    title3='これはタイトルです?'
-    content3='これはコンテンツです。これはコンテンツです。これはコンテンツです。これはコンテンツです。これはコンテンツです。これはコンテンツです。これはコンテンツです。'
-    />
-  </div>
+    <div className='mx-auto max-w-4xl mt-10'>
+      <div className='flex justify-center'>
+        <div>
+          <h2 className='text-6xl mb-4 text-center'>{count}</h2>
+          <Button 
+            onClick={onClickCountUp} 
+            label="カウントアップ" 
+            variant="primary"
+          />
+        </div>
+      </div>
+    </div>
   );
 };
 
