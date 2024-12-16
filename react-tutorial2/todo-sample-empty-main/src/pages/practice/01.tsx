dc14b442de6cd8de75830bf7fa2782df2180cd0d
kanami_suzuki <58844108+kanami-suzuki@users.noreply.github.com>
2024-12-16 22:50:49 +0900

    テキストの表示非表示

--- react-tutorial2/todo-sample-empty-main/src/pages/practice/01.tsx
+++ react-tutorial2/todo-sample-empty-main/src/pages/practice/01.tsx
@@ -1,22 +1,29 @@
 import { NextPage } from 'next';
-import { useState } from 'react';
 
 import Button from '@/components/common/parts/Button';
-import { useCountUp } from '@/lib/useCountUp';
+import { useState } from 'react';
 
 const Page: NextPage = () => {
-  const {count, onClickCountUp} = useCountUp()
+  const [isHidden, setIsHidden] = useState(false);
+
+  const handleClickIndicate = () => {
+    setIsHidden(false);
+  }
+
+  const handleClickHidden = () => {
+    setIsHidden(true);
+  }
 
   return( 
     <div className='mx-auto max-w-4xl mt-10'>
       <div className='flex justify-center'>
         <div>
-          <h2 className='text-6xl mb-4 text-center'>{count}</h2>
-          <Button 
-            onClick={onClickCountUp} 
-            label="カウントアップ" 
-            variant="primary"
-          />
+          {!isHidden && <h2 className='mb-4 text-6xl'>こんにちは</h2>}
+
+          <div className='flex justify-center gap-x-4'>
+            <Button onClick={handleClickIndicate} label='表示' variant='primary'/>
+            <Button onClick={handleClickHidden} label='非表示' variant='primary'/>
+          </div>
         </div>
       </div>
     </div>
