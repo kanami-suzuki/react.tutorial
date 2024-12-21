8ca58018a8afe3de1bfcd5b97b6922639b3925b7
kanami_suzuki <58844108+kanami-suzuki@users.noreply.github.com>
2024-12-21 23:36:55 +0900

    テキスト表示

--- react-tutorial2/todo-sample-empty-main/src/pages/practice/01.tsx
+++ react-tutorial2/todo-sample-empty-main/src/pages/practice/01.tsx
@@ -1,20 +1,23 @@
 import { NextPage } from 'next';
 
 import Button from '@/components/common/parts/Button';
-import { useTextShowHidden } from '@/lib/useTextShowHidden';
+import { useState } from 'react';
+// import { useTextShowHidden } from '@/lib/useTextShowHidden';
 
 const Page: NextPage = () => {
-  const {isHidden, handleClickIndicate, handleClickHidden} = useTextShowHidden();
+  const [text, setText] = useState('');
+
+  const handleChengeText = (e: ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  }
 
   return( 
     <div className='mx-auto max-w-4xl mt-10'>
       <div className='flex justify-center'>
         <div>
-          {!isHidden && <h2 className='mb-4 text-6xl'>こんにちは</h2>}
-
-          <div className='flex justify-center gap-x-4'>
-            <Button onClick={handleClickIndicate} label='表示' variant='primary'/>
-            <Button onClick={handleClickHidden} label='非表示' variant='primary'/>
+          <h2 className='mb-4 text-center text-6xl'>{text}</h2>
+          <div className='flex justify-center'>
+            <input onChange={handleChengeText} className='border px-3 py-2' type="text" />
           </div>
         </div>
       </div>
